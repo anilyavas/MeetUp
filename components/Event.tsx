b03d0f35f73afc146034344a89f289c4f4d9facf
anilyavas <anil.yavas99@gmail.com>
2024-09-11 21:23:47 +0300

    display dates with dayjs

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,18 +1,19 @@
 import { Feather } from '@expo/vector-icons';
+import dayjs from 'dayjs';
 import { View, Text, Image } from 'react-native';
 
 export default function EventListItem({ event }) {
   return (
-    <View className="gap-3 p-3">
+    <View className="m-3 gap-3 border-b-2 border-gray-100 pb-3">
       <View className="flex-row">
         <View className="flex-1 gap-2">
           <Text className="text-lg font-semibold uppercase text-amber-800">
-            Wed 13, Sep · 19:30 CEST
+            {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
           </Text>
           <Text className="text-xl font-bold" numberOfLines={2}>
             {event.title}
           </Text>
-          <Text className="text-grey-700">{event.location}</Text>
+          <Text className="text-gray-700">{event.location}</Text>
         </View>
         {/* Event image */}
         <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
@@ -20,8 +21,8 @@ export default function EventListItem({ event }) {
       {/* Footer */}
       <View className="flex-row gap-3">
         <Text className="text-grey-700 mr-auto">16 going</Text>
-        <Feather name="share" size={20} color="grey" />
-        <Feather name="bookmark" size={20} color="grey" />
+        <Feather name="share" size={20} color="gray" />
+        <Feather name="bookmark" size={20} color="gray" />
       </View>
     </View>
   );
