1a6d78ac7ac5131d8e9b701767b9ee8e48181f90
anilyavas <anil.yavas99@gmail.com>
2024-09-12 20:05:04 +0300

    RSVP Footer

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Pressable } from 'react-native';
 
 import events from '../assets/events.json';
 
@@ -24,6 +24,12 @@ export default function EventPage() {
         {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
       </Text>
       <Text className="text-lg">{event.description}</Text>
+      <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-400 p-5 pb-10">
+        <Text className=" text-xl font-bold">Free</Text>
+        <Pressable className="rounded-md bg-red-500 p-5 px-8">
+          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
