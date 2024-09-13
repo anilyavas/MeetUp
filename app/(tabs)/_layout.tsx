d85fbf8cd789f76d5a2dd62ba2d1566a19bbc412
anilyavas <anil.yavas99@gmail.com>
2024-09-13 13:51:42 +0300

    authentication and screen protection

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,10 +3,15 @@ import { Link, Redirect, Tabs } from 'expo-router';
 import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
 
+import { useAuth } from '~/context/AuthProvider';
+
 export default function TabLayout() {
-  return <Redirect href="/login" />;
-  {
-    /*return (
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Redirect href="/login" />;
+  }
+  return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: 'black',
@@ -31,6 +36,5 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
-  );*/
-  }
+  );
 }
