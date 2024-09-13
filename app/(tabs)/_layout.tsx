7266604e543a583b8a88d66d47978119e3797951
anilyavas <anil.yavas99@gmail.com>
2024-09-13 13:33:46 +0300

    auth screen and logic

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,12 @@
-import { Link, Tabs } from 'expo-router';
+import { Link, Redirect, Tabs } from 'expo-router';
 
 import { HeaderButton } from '../../components/HeaderButton';
 import { TabBarIcon } from '../../components/TabBarIcon';
 
 export default function TabLayout() {
-  return (
+  return <Redirect href="/login" />;
+  {
+    /*return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: 'black',
@@ -29,5 +31,6 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
-  );
+  );*/
+  }
 }
